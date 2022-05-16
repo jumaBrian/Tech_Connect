e306b0b31ded614a0c23fb61cd1f80c210309699
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-05-16 17:59:17 +0300

    Add member variables for Professional class.

--- src/main/java/models/Professional.java
+++ src/main/java/models/Professional.java
@@ -1,4 +1,14 @@
 package models;
 
+import java.util.Objects;
+
 public class Professional {
+
+    private String professional_name;
+    private String email;
+    private String qualification;
+    private String bio;
+    private int service_id;
+    private int id;
+
 }
