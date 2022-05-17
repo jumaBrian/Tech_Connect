873a2ce22599d80708fc5bd560dc8c5879f47dff
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-05-17 09:30:28 +0300

    Add professional dao interface.

--- src/main/java/interfaces/ProfessionalDao.java
+++ src/main/java/interfaces/ProfessionalDao.java
@@ -1,4 +1,21 @@
 package interfaces;
 
+import models.Professional;
+
+import java.util.List;
+
 public interface ProfessionalDao {
+    //create
+    void add (Professional professional);
+
+    //read
+    List<Professional> getAllProfessionals();
+    Professional findProfessionalById(int id);
+
+    //update
+    void update(int id, String professional_name, String bio, String qualification,String email, int service_id );
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
