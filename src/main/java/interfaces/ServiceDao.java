7f11637a8d4b3cf99ef9d3a676db3d76352b24f5
MutheuLinet <muneenilinet@gmail.com>
2022-05-17 15:21:07 +0300

    Add service model interface methods

--- src/main/java/interfaces/ServiceDao.java
+++ src/main/java/interfaces/ServiceDao.java
@@ -1,4 +1,19 @@
 package interfaces;
 
+import models.Service;
+
+import java.util.List;
+
 public interface ServiceDao {
+    //CRUD
+    //create
+    void add(Service service);
+
+    //read
+    List<Service> getAll();
+
+    //update
+    void update(Service service);
+    //delete
+    void delete(int id);
 }
