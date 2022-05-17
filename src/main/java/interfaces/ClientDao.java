fc6aad45abd6d2008e82cf9ff5a5c80c5085ccd9
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-05-17 13:22:18 +0300

    Add  client dao interface with method signature to methods that will be implemented

--- src/main/java/interfaces/ClientDao.java
+++ src/main/java/interfaces/ClientDao.java
@@ -1,4 +1,25 @@
 package interfaces;
 
+import models.Client;
+
+import java.util.List;
+
 public interface ClientDao {
+    //CRUD
+    //create
+    void add(Client client);
+
+    //Read
+    List<Client> getAll();
+
+    //Find
+    Client findById(int id);
+
+    //Update
+    void update(int id, String name, String email, int contact);
+
+    //Destroy
+    void deleteById(int id);
+//    void clearAll();
+
 }
