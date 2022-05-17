891961ed16c2db49ece117f7d6922a14546a020f
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-05-17 09:36:40 +0300

    Add boilerplate to tests file.

---
+++ src/test/java/sql2o/Sql2oClientDaoTest.java
@@ -0,0 +1,17 @@
+package sql2o;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Sql2oClientDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
