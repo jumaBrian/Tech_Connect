7dcb37bbca6f61b9dd8f0d0245f63afca767d73b
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-05-17 13:21:29 +0300

    Add POJO class member variables with getters and setters

--- src/main/java/models/Client.java
+++ src/main/java/models/Client.java
@@ -1,4 +1,70 @@
 package models;
 
+import java.util.Objects;
+
 public class Client {
+    private String name;
+    private String email;
+    private int contact;
+    private int id;
+
+    public Client(String name, String email, int contact) {
+        this.name = name;
+        this.email = email;
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public void setContact(int contact) {
+        this.contact = contact;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Client client = (Client) o;
+
+        if (contact != client.contact) return false;
+        if (id != client.id) return false;
+        if (!Objects.equals(name, client.name)) return false;
+        return Objects.equals(email, client.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + contact;
+        result = 31 * result + id;
+        return result;
+    }
 }
