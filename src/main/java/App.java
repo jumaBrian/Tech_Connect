6294cebe74d76cef60846c90bce8f298fe80bb44
jackie <bobbinnej@gmail.com>
2022-05-18 13:04:39 +0300

    Create routes to take us to different sections

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,28 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
+        //route to take us to homepage
+        get("/",(request,respond)->{
+            Map<String,Object>model=new HashMap<>();
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        //route for get started button to take us to about page
+        get("/about",(request,respond)->{
+            Map<String,Object>model=new HashMap<>();
+            return new ModelAndView(model,"about.hbs");
+        }, new HandlebarsTemplateEngine());
+
 
     }
 }
