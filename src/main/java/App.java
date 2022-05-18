0303ce359dc52663e4d724602b124c1b54320160
jackie <bobbinnej@gmail.com>
2022-05-18 15:52:03 +0300

    Add navigation routes,contact.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,7 @@ import static spark.Spark.staticFileLocation;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
         //route to take us to homepage
         get("/",(request,respond)->{
             Map<String,Object>model=new HashMap<>();
@@ -17,10 +18,22 @@ public class App {
         }, new HandlebarsTemplateEngine());
 
 
-        //route for get started button to take us to about page
+        //route for about navbar to take us to about page
         get("/about",(request,respond)->{
             Map<String,Object>model=new HashMap<>();
             return new ModelAndView(model,"about.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //route for services navbar text to take us to services page
+        get("/services",(request,respond)->{
+            Map<String,Object>model=new HashMap<>();
+            return new ModelAndView(model,"services.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //route for contacts navbar text to take us to contact page
+        get("/contacts",(request,respond)->{
+            Map<String,Object>model=new HashMap<>();
+            return new ModelAndView(model,"contacts.hbs");
         }, new HandlebarsTemplateEngine());
 
 
