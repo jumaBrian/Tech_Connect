9303532362464c0162072ec0a295f81bf3296053
jackie <bobbinnej@gmail.com>
2022-05-18 10:47:48 +0300

    Edit page font to quicksand

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,6 @@
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
-
-import static spark.Spark.*;
-import static spark.Spark.post;
 
 public class App {
     public static void main(String[] args) {
-        staticFileLocation("/public");
-
-        get("/", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
-
 
     }
 }
